2eadc585314ef3843906956a039940d095539bbc
daisuke-hara0515 <wildlife402@gmail.com>
2021-05-29 13:45:23 +0900

    ログインする際の入力欄作成

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,6 +1,39 @@
 <template>
     <div>
-        <h3>Login</h3>
-        <button>ログイン</button>
+        <header>
+            アプリ名
+        </header>
+        <hr>
+        <div class="container">
+            <label for="username">ユーザー名:</label>
+            <input id="username"
+                type="username"
+                v-model="username"
+            >
+            <br><br>
+            <label for="password">パスワード:</label>
+            <input id="password"
+                type="password"
+                v-model="password"
+            >
+                
+            <h3>
+                新規ユーザーはこちらから
+            </h3>
+            <h3>
+                パスワードを忘れてしまったら
+            </h3>
+
+        </div>
     </div>
 </template>
+
+<style scoped>
+.container{
+    width:400px;
+    height:300px;
+    position: absolute;
+    top:100px;
+    right: 0px;
+}
+</style>
