fac1f5dee529eb4443d65ddf4275f54f1f66740b
daisuke-hara0515 <wildlife402@gmail.com>
2021-05-30 11:36:55 +0900

    認証トークンが有効かどうか確認するコード追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  # 認証トークンが有効かどうかを確認する
+  client = request.headers['client']
+  token = request.headers['access-token']
+
+  @resource.valid_token?(token,client)
 end
