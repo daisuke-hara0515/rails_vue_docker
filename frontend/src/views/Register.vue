174a6c6c470916181f59bc5bea8d1c377120f96e
daisuke-hara0515 <wildlife402@gmail.com>
2021-06-05 13:16:05 +0900

    login view copy

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
-        <h3>登録</h3>
+        <div class="container">
+            <label for="email">Email</label>
+            <br>
+            <input id="email"
+                type="email"
+                v-model="email"
+            >
+            <br><br>
+            <label for="password">パスワード</label>
+            <br>
+            <input id="password"
+                type="password"
+                v-model="password"
+            >
+            <br><br>
+            <button @click="login">ログイン</button>
+            <button>新規ユーザー登録はこちらから</button>
+            <br><br>
+            <button>パスワードを忘れてしまったら</button>
+        </div>
     </div>
 </template>
