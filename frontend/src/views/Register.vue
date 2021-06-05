e8a5fd56388860f9d9f0a0c8b830893e51ac0e56
daisuke-hara0515 <wildlife402@gmail.com>
2021-06-05 22:24:29 +0900

    login viewへのリンク作成

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,21 +1,28 @@
 <template>
     <div>
-        <div class="container">
-            <label for="email">Email</label>
-            <br>
-            <input id="email"
-                type="email"
-                v-model="email"
-            >
-            <br><br>
-            <label for="password">パスワード</label>
-            <br>
-            <input id="password"
-                type="password"
-                v-model="password"
-            >
-            <br><br>
-            <button @click="createUser">ユーザー登録</button>
+        <header>
+            アプリ名
+            <router-link to="/login">ログイン画面に戻る</router-link>
+        </header>
+        <hr>
+        <div>
+            <div class="container">
+                <label for="email">Email</label>
+                <br>
+                <input id="email"
+                    type="email"
+                    v-model="email"
+                >
+                <br><br>
+                <label for="password">パスワード</label>
+                <br>
+                <input id="password"
+                    type="password"
+                    v-model="password"
+                >
+                <br><br>
+                <button @click="createUser">ユーザー登録</button>
+            </div>
         </div>
     </div>
 </template>
@@ -47,3 +54,13 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.container{
+    width:400px;
+    height:300px;
+    position: absolute;
+    top:250px;
+    right: 400px;
+}
+</style>
