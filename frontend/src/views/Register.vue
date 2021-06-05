6a98f946b3a9ed9eabdb5f6815c04ccf9e03300e
daisuke-hara0515 <wildlife402@gmail.com>
2021-06-05 13:33:07 +0900

    register機能追加

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -15,8 +15,35 @@
                 v-model="password"
             >
             <br><br>
-            <button @click="createUser">ユーザー作成</button>
+            <button @click="createUser">ユーザー登録</button>
         </div>
     </div>
 </template>
 
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        createUser() {
+            axios.post('http://localhost:3000/v1/auth',
+            {
+                email: this.email,
+                password: this.password
+            }).then(response => {
+                console.log(response);
+            }).catch(error => {
+                console.log(error);
+            });
+            this.email = "",
+            this.password = ""
+        }
+    }
+}
+</script>
