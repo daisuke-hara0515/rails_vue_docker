aea6a5e363f4163da55fac09c38b1b3374bc0a20
daisuke-hara0515 <wildlife402@gmail.com>
2021-06-23 23:05:40 +0900

    exposeに関するメモ追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :delete]
+      # exposeはレスポンスのHTTPヘッダの公開を許可するかどうかを指定する
+      expose: ['access-token','expiry','token-type','uid','client'],
+      methods: [:get, :post, :options, :delete, :put, :patch, :head]
   end
 end
