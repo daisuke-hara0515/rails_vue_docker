ebc360420e425603cfab5b0bb90eb9c4170de072
daisuke-hara0515 <wildlife402@gmail.com>
2021-06-26 13:11:26 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_230953) do
+ActiveRecord::Schema.define(version: 2021_06_26_014942) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.text "description"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
