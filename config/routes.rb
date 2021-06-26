47324487b13426e2d26bd44a77a7507fc95c5254
daisuke-hara0515 <wildlife402@gmail.com>
2021-06-26 12:57:48 +0900

    json形式のリクエストに対応するように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks
+  # json形式のリクエストに対応
+  scope format: 'json' do
+    resources :tasks
+  end
+
   namespace :v1 do
     mount_devise_token_auth_for 'User', at: 'auth'
   end
