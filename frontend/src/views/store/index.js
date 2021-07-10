4592a6103ef4c1e13fe2ac928838eaeebedff4a3
daisuke-hara0515 <wildlife402@gmail.com>
2021-07-10 13:37:02 +0900

    index.jsにstateとmutationsを用意

--- frontend/src/views/store/index.js
+++ frontend/src/views/store/index.js
@@ -3,4 +3,13 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({});
+export default new Vuex.Store({
+    state: {
+        idToken: null
+    },
+    mutations: {
+        updateIdToken(state,idToken) {
+            state.idToken = idToken;
+        }
+    }
+});
