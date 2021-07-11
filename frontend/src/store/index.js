889043732c34a5548927b997167269625049b8df
daisuke-hara0515 <wildlife402@gmail.com>
2021-07-11 11:21:24 +0900

    idTokenがある時は管理画面にリダイレクトする

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from "axios";
+import router from '../router'
 
 Vue.use(Vuex);
 
@@ -25,6 +26,7 @@ export default new Vuex.Store({
                 returnSecureToken: true
             }).then(response => {
                 commit('updateIdToken',response.data.idToken)
+                router.push('/')
                 console.log(response);
             }).catch(error => {
                 console.log(error);
