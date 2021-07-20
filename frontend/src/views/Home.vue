ced2cee9694dcca510bd9e9a871d2cf310504801
daisuke-hara0515 <wildlife402@gmail.com>
2021-07-20 23:09:43 +0900

    ログアウトにカーソルを合わせてクリックできるようにした

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         <h3>管理画面</h3>
-        <span @click="logout">ログアウト</span>
+        <span class="header-item" @click="logout">ログアウト</span>
     </div>
 </template>
 
+<style scoped>
+.header-item {
+    cursor: pointer;
+}
+</style>
