87b0a2817c040062a041b22995ac1cb99dd4f758
daisuke-hara0515 <wildlife402@gmail.com>
2021-07-24 22:55:12 +0900

    taskcontrollerとrouteに追記

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
 
   namespace :v1 do
     mount_devise_token_auth_for 'User', at: 'auth'
+    resources :tasks, only: %i(index)
+    namespace 'auth' do
+      post 'registrations' => 'registrations#create'
+    end
   end
 end
