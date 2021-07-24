702a0c3082ce55c0317bd12ef0e96854bc75eadf
daisuke-hara0515 <wildlife402@gmail.com>
2021-07-24 11:37:14 +0900

    createメソッドでGoogle x509証明書ダウンロードする

--- app/controllers/v1/auth/registrations_controller.rb
+++ app/controllers/v1/auth/registrations_controller.rb
@@ -2,5 +2,8 @@ class V1::Auth::RegistrationsController < DeviceTokenAuth::RegistrationsControll
     # 定義したbefore_actionを使いたくない時はskip_before_action
     skip_before_action :authenticate_user
 
+    def create
+        # Googleのx509証明書をダウンロードする
+        FirebaseIdToken::Certificates.request
 
 end
