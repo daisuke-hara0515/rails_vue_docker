15b5f37675e51547686f2dff94d9db6be3c47fb8
daisuke-hara0515 <wildlife402@gmail.com>
2021-07-24 11:21:44 +0900

    skip_before_actionを設定

--- app/controllers/v1/auth/registrations_controller.rb
+++ app/controllers/v1/auth/registrations_controller.rb
@@ -1,2 +1,6 @@
 class V1::Auth::RegistrationsController < DeviceTokenAuth::RegistrationsController
+    # 定義したbefore_actionを使いたくない時はskip_before_action
+    skip_before_action :authenticate_user
+
+
 end
