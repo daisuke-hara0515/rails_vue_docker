2f71d18174ffc17d1e146aac328c0a52529d22f7
daisuke-hara0515 <wildlife402@gmail.com>
2021-08-22 19:22:33 +0900

    createメソッドを定義

--- app/controllers/v1/auth/tasks_controller.rb
+++ app/controllers/v1/auth/tasks_controller.rb
@@ -4,6 +4,16 @@ module V1
       def index
         render json: current_user.tasks, status: 200
       end
+
+      def create
+        @task = Task.new(task_params)
+        if @task.save
+          render json: "create new task.\n", status: 200
+        else
+          render json: "fail to create. \n", status: 500
+        end
+      end
+      
     end
   end
 end
