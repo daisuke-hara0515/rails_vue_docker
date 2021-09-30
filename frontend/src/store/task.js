645303f4deb82e243f1c84ce6d3ef97f134dfeb1
daisuke-hara0515 <wildlife402@gmail.com>
2021-09-30 23:36:37 +0900

    mutationsを設定

--- frontend/src/store/task.js
+++ frontend/src/store/task.js
@@ -3,9 +3,19 @@ import axios from 'axios';
 const apiUrl = 'http://localhost:3000';
 
 const state = {
-    task:[]
+    tasks:[]
 };
 
 const getters = {
-    task: task => state.task
+    tasks: tasks => state.tasks
+}
+
+const mutations = {
+    setTasks: (state, tasks) => (state.tasks = tasks)
+}
+
+const actions = {
+    async fetchTasks({commit}) {
+        const response = await axios.get(`${apiUrl}/`)
+    }
 }
