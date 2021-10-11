33fc296eb02c54a3f20d1b8302fc1d1fddc78ee7
daisuke-hara0515 <wildlife402@gmail.com>
2021-10-11 18:31:09 +0900

    task.jsのコードを一度コメントアウトする(Vueファイルに記述してから徐々に移していく)

--- frontend/src/store/task.js
+++ frontend/src/store/task.js
@@ -1,21 +1,22 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const apiUrl = 'http://localhost:3000';
+// const apiUrl = 'http://localhost:3000';
 
-const state = {
-    tasks:[]
-};
+// const state = {
+//     tasks:[]
+// };
 
-const getters = {
-    tasks: tasks => state.tasks
-}
+// const getters = {
+//     tasks: tasks => state.tasks
+// }
 
-const mutations = {
-    setTasks: (state, tasks) => (state.tasks = tasks)
-}
+// const mutations = {
+//     setTasks: (state, tasks) => (state.tasks = tasks)
+// }
 
-const actions = {
-    async fetchTasks({commit}) {
-        const response = await axios.get(`${apiUrl}/`)
-    }
-}
+// const actions = {
+//     async fetchTasks({commit}) {
+//         const response = await axios.get(`${apiUrl}/v1/auth/tasks`);
+//         console.log(response.data);
+//     }
+// }
